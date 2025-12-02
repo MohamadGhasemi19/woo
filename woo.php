e810c039823814a9e8f8da05429afcea845ff945
mohamad <mohamad@mohamads-MacBook-Air.local>
2025-12-02 13:48:45 +0330

    Adds the Featured Badge meta box to WooCommerce products

--- woo.php
+++ woo.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       Woo
  * Plugin URI:        https://#
- * Description:       This is a description of the pluginniuniuniui.
+ * Description:       This is a description of the plugin.
  * Version:           1.0.0
  * Author:            a
  * Author URI:        https://#/
@@ -80,3 +80,64 @@ function run_woo() {
 
 }
 run_woo();
+
+
+ /*Adds the Featured Badge meta box to WooCommerce products.*/
+ 
+ 
+function add_featured_badge_meta_box() {
+    add_meta_box(
+        'featured_badge_meta_box',
+        __( 'Featured Badge', 'your-plugin-textdomain' ),
+        'render_featured_badge_meta_box',
+        'product',
+        'side',
+        'high'
+    );
+}
+add_action( 'add_meta_boxes', 'add_featured_badge_meta_box' );
+
+
+function render_featured_badge_meta_box( $post ) {
+
+	
+    wp_nonce_field( 'your_plugin_nonce', 'your_plugin_nonce_field' );
+
+
+	$is_featured = get_post_meta( $post->ID, '_is_featured_custom', true );
+    ?>
+    <label for="is_featured_custom">
+        <input type="checkbox"
+               name="is_featured_custom"
+               id="is_featured_custom"
+               value="1"
+               <?php checked( $is_featured, '1' ); ?> />
+        <?php _e( 'Enable Featured Badge', 'your-plugin-textdomain' ); ?>
+    </label>
+    <?php
+}
+
+
+function your_plugin_save_data( $post_id ) {
+
+    
+    if ( ! isset( $_POST['your_plugin_nonce'] ) || ! wp_verify_nonce( $_POST['your_plugin_nonce'], 'your_plugin_nonce_field' ) ) {
+        return;
+    }
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    if ( ! current_user_can( 'manage_options' ) ) {
+        return;
+    }
+
+    
+    if ( isset( $_POST['is_featured_custom'] ) ) {
+        update_post_meta( $post_id, '_is_featured_custom', '1' );
+    } else {
+        delete_post_meta( $post_id, '_is_featured_custom' );
+    }
+}
+add_action( 'save_post_product', 'your_plugin_save_data' );
